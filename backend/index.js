994302fbf0ca4046e037306a31d202329b656477
Trinh Son <caoson.trinh@gmail.com>
2022-04-16 21:13:35 +0300

    - Change backend port - Allow cross origin request with cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
-const port = 3000;
+const port = 5000;
 
 server.listen(port, () => {
   console.log(`API listening on port ${port}`);
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routes = require("../routes");
+const cors = require("cors");
 
 const server = express();
 
+server.use(cors());
 server.use("/", routes);
 
 module.exports = server;
